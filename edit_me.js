656d46078c064d6d65c5f118594b1caf0281dff9
Ryan <ryan314r@gmail.com>
2017-02-13 19:39:59 -0600

    Add solutions

--- edit_me.js
+++ edit_me.js
@@ -31,3 +31,18 @@ function power(a, b) {
 function round(num) {
 	return Math.round(num * 100) / 100;
 }
+
+// add support for percents
+function parseNumber(num) {
+	// if the number ends with a % sign treat it as a percent
+	if(num[num.length - 1] == "%") {
+		// remove the % by getting every part of the string except the percent
+		num = num.substr(0, num.length - 1);
+
+		// parse the numbers and divide by 100 to get the percent value
+		return parseFloat(num) / 100;
+	}
+
+	// parse any other numbers normally
+	return parseFloat(num);
+}
